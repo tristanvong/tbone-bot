59e86fd05b6d41e5a65ac1626933727a0aa06e37
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-05-02 18:45:43 +0200

    singleton bot

---
+++ src/main/java/be/tristan/tbone_bot/main/Main.java
@@ -0,0 +1,9 @@
+package be.tristan.tbone_bot.main;
+
+import be.tristan.tbone_bot.config.BotConfiguration;
+
+public class Main {
+    public static void main(String[] args) {
+        BotConfiguration.getInstance();
+    }
+}
